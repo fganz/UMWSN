e46135bb686e4c6eebe8ef11e2298cf5f0965d6e
frieder <fganz@blorg.de>
2012-05-01 19:09:24 +0100

    Setup Tool

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='UMWSN',
+    version='0.1',
+    packages=['core', 'core.plugins', 'core.plugins.processing', 'core.plugins.abstraction'],
+    url='https://github.com/fganz/UMWSN',
+    license='LGPL',
+    author='',
+    author_email='f.ganz@surrey.ac.uk',
+    description=''
+)
