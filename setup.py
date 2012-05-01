c4fe993b64a1a6ba8358f6cf62e83ae0a4b5870d
frieder <fganz@blorg.de>
2012-05-01 19:53:36 +0100

    Setup Tool

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='UMWSN',
+    name='UMiddlewareWSNetworks',
     version='0.1',
     packages=['core', 'core.plugins', 'core.plugins.processing', 'core.plugins.abstraction'],
     url='https://github.com/fganz/UMWSN',
     license='LGPL',
-    author='',
+    author='Frieder Ganz',
     author_email='f.ganz@surrey.ac.uk',
-    description=''
+    description='Middleware for Wireless Sensor Networks',
+    install_requires=['pandas','mako','numpy','pymongo','cherrypy','matplotlib','scipy','googlemaps','pywapi']
 )
